8dc5a0b96d34c430b7254b5cc5ebf4973fd4ff29
Nahdus <b.j.ponsudhan@gmail.com>
2017-07-01 20:14:14 +0530

    Update settings.py

--- nahduslab/nahduslab/settings.py
+++ nahduslab/nahduslab/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '68c(3t4@!8qy)qugf^#%#nx^vvzormvqe!92t)!o^(2zoout41'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
